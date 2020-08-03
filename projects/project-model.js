9dd2202b97ffe20db38630957cde93ba9debddf2
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-08-02 20:50:29 -0700

    mvp met

--- projects/project-model.js
+++ projects/project-model.js
@@ -10,25 +10,30 @@ module.exports = {
 }
 
 function getProjects(){
-
+    return db('projects');
 }
 
 function getResources(){
-
+    return db('resources');
 }
 
 function getTasks(){
-
+    return db('projects as p')
+    .join('tasks as t', 't.project_id', 'p.id')
+    .select('t.description as task', 't.notes', 't.completed', 'p.name as project', 'p.description');
 }
 
 function addProject(project){
-
+    return db('projects')
+    .insert(project);
 }
 
 function addResource(resource){
-
+    return db('resources')
+    .insert(resource);
 }
 
 function addTask(task){
-    
+    return db('tasks')
+    .insert(task);
 }
--- projects/project-router.js
+++ projects/project-router.js
@@ -26,5 +26,50 @@ router.get('/resources', (req,res)=>{
     })
 })
 
+router.get('/tasks', (req, res)=>{
+    Projects.getTasks()
+    .then(tasks =>{
+        res.status(200).json(tasks);
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({message: 'unable to get tasks'})
+    })
+})
+
+router.post('/projects', (req,res)=>{
+    const project = req.body;
+    Projects.addProject(project)
+    .then(project =>{
+        res.status(201).json(project);
+    })
+    .catch(err=>{
+        res.status(500).json({message: 'unable to add project'})
+    })
+})
+
+router.post('/resources', (req,res)=>{
+    const resource = req.body;
+    Projects.addResource(resource)
+    .then(resource =>{
+        res.status(201).json(resource);
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({message: 'unable to post resource'})
+    })
+})
+
+router.post('/tasks', (req,res)=>{
+    const task = req.body;
+    Projects.addTask(task)
+    .then(task=>{
+        res.status(201).json(task);
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({message: 'unable to post task'})
+    })
+})
 
 module.exports = router;
